662ca32228dfa8df62f42c4675cc0d5b1908213c
HuynhBaoNgoc <hiudahuynh@gmail.com>
2023-11-23 14:07:47 +0700

    implement unit test

--- PandaAPIUnitTesting/ControllerTests/CityControllerTests.cs
+++ PandaAPIUnitTesting/ControllerTests/CityControllerTests.cs
@@ -2,54 +2,151 @@
 using FakeItEasy;
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
-using Mysqlx;
+using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities.Resources;
+using Moq;
 using PandaAPI.Controllers;
+using PandaApplication.Services;
 using PandaApplication.Services.Interfaces;
+using PandaDomain.Entities;
+using PandaDomain.Mappings;
 using PandaDomain.Models.Request;
 using PandaDomain.Models.Response;
+using System.Net;
 
 namespace PandaAPIUnitTesting.ControllerTests
 {
     public class CityControllerTests
     {
-        private readonly ICityService _cityServices;
-        private readonly IMapper _mapper;
+        private readonly Mock<ICityService> _cityService;
+        private CityController _controller;
 
-        public CityControllerTests() {
-            _cityServices = A.Fake<ICityService>();
-            _mapper = A.Fake<IMapper>();
+        public CityControllerTests()
+        {
+            _cityService = new Mock<ICityService>();
+            _controller = new CityController(_cityService.Object);
         }
 
         [Fact]
-        public async void CityController_GetListCity_ReturnOK()
+        public async void GetListCity_ReturnsListOfCities()
         {
-            //Arrange
-            var cities = A.Fake<ICollection<CityResponse>>();
-            var cityList = A.Fake<List<CityResponse>>();
-            A.CallTo(() => _mapper.Map<List<CityResponse>>(cities)).Returns(cityList);
-            var controller = new CityController(_cityServices);
-            //Act
-            var result = await controller.GetListCity();
-            //Assert
-            result.Should().NotBeNull();
-            result.Result.Should().BeOfType<OkObjectResult>(); // Kiểm tra kiểu là OkObjectResult
+            // Arrange
+            var expectedCities = GetSampleCityList();
+            _cityService.Setup(x => x.GetListCity()).ReturnsAsync(expectedCities);
+
+            // Act
+            var result = await _controller.GetListCity();
+
+            // Assert
+            Assert.NotNull(result);
+
+            var okResult = Assert.IsType<ActionResult<List<CityResponse>>>(result);
+            var actualCities = Assert.IsType<OkObjectResult>(okResult.Result).Value;
+
+            // Add more specific assertions based on your requirements
+            Assert.IsType<List<CityResponse>>(actualCities);
+            Assert.Equal(expectedCities.Count, (actualCities as List<CityResponse>).Count);
         }
+
         [Fact]
-        public async void CityController_AddCity_ReturnOK()
+        public async void AddCity_ReturnsOkResultWithListOfCities()
         {
-            //Arrange
+            // Arrange
+            var cityRequest = new AddCityRequest
+            {
+                CityName = "City3"
+            };
+
+            var expectedCities = GetSampleCityList().Append(new CityResponse
+            {
+                CityId = 3,
+                CityName = "City3"
+            }).ToList();
+
+            _cityService.Setup(x => x.AddCity(cityRequest)).ReturnsAsync(expectedCities);
+
+            // Act
+            var result = await _controller.AddCity(cityRequest);
+
+            // Assert
+            Assert.NotNull(result);
+            var okResult = Assert.IsType<ActionResult<List<CityResponse>>>(result);
+            var actualCitiesResponse = Assert.IsType<OkObjectResult>(okResult.Result).Value;
+
+            // Add more specific assertions based on your requirements
+            Assert.IsType<List<CityResponse>>(actualCitiesResponse);
+            Assert.Equal(expectedCities, actualCitiesResponse as List<CityResponse>);
+        }
+
+        [Fact]
+        public async void UpdateCity_ReturnsOkResultWithCityResponse()
+        {
+            // Arrange
+            var cityRequest = new UpdateCityRequest
+            {
+                CityId = 1,
+                CityName = "City One"
+            };
+
+            var expectedCity = new CityResponse
+            {
+                CityId = 1,
+                CityName = "City One"
+            };
+
+            _cityService.Setup(x => x.UpdateCity(cityRequest)).ReturnsAsync(expectedCity);
+
+            // Act
+            var result = await _controller.UpdateCity(cityRequest);
+
+            // Assert
+            Assert.NotNull(result);
+            var okResult = Assert.IsType<ActionResult<CityResponse>>(result);
+            var actualCityResponse = Assert.IsType<OkObjectResult>(okResult.Result).Value;
+
+            // Add more specific assertions based on your requirements
+            Assert.IsType<CityResponse>(actualCityResponse);
+            Assert.Equal(expectedCity.CityId, (actualCityResponse as CityResponse).CityId);
+            Assert.Equal(expectedCity.CityName, (actualCityResponse as CityResponse).CityName);
+        }
+
+        [Fact]
+        public async void DeleteCity_ReturnsOkResultWithListOfCities()
+        {
             int cityId = 1;
-            string cityName = "Trung Khanh";
-            var cityRequest = A.Fake<AddCityRequest>();
-            var cities = A.Fake<ICollection<CityResponse>>();
-            var cityList = A.Fake<List<CityResponse>>();
-            A.CallTo(() => _mapper.Map<List<CityResponse>>(cities)).Returns(cityList);
-            var controller = new CityController(_cityServices);
-            //Act
-            var result = await controller.GetListCity();
-            //Assert
-            result.Should().NotBeNull();
-            result.Result.Should().BeOfType<OkObjectResult>(); // Kiểm tra kiểu là OkObjectResult
+            // Arrange
+            var sampleCityList = GetSampleCityList();
+            var expectedCities = sampleCityList.Where(city => city.CityId != 1).ToList();
+
+            _cityService.Setup(x => x.Delete(cityId)).ReturnsAsync(expectedCities);
+
+            // Act
+            var result = await _controller.Delete(cityId);
+
+            // Assert
+            Assert.NotNull(result);
+            var okResult = Assert.IsType<ActionResult<List<CityResponse>>>(result);
+            var actualCitiesResponse = Assert.IsType<OkObjectResult>(okResult.Result).Value;
+
+            // Add more specific assertions based on your requirements
+            Assert.IsType<List<CityResponse>>(actualCitiesResponse);
+            Assert.Equal(expectedCities, actualCitiesResponse as List<CityResponse>);
         }
+
+        #region Setup
+        public static List<CityResponse> GetSampleCityList()
+        {
+            return new List<CityResponse>
+            {
+                new CityResponse { CityId = 1, CityName = "City1" },
+                new CityResponse { CityId = 2, CityName = "City2" }
+                // Add more city responses as needed
+            };
+        }
+
+        //private void Setup()
+        //{
+        //    var config = new MapperConfiguration(cfg => cfg.AddProfile<MappingProfile>());
+        //}
+        #endregion
     }
 }
